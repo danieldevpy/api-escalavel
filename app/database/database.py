cdd7ba09434386bd848041c93e8aac29d594318c
Daniel <danielfernandes202@gmail.com>
2023-02-26 14:29:07 -0300

    adicionando banco mysql

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import time
 
-
+time.sleep(2)
 # Database creation
-engine = create_engine("mysql+mysqlconnector://root:root@192.168.10.18:3307/dbgo")
+engine = create_engine("mysql+mysqlconnector://api_user:api_pass@192.168.10.18:3307/dbapi")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
